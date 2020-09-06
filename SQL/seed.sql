1f5282a97a84e16b21666fb40a0229e158ee0db0
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-05 23:40:44 -0400

    added values into tables on seed.sql file

--- SQL/seed.sql
+++ SQL/seed.sql
@@ -5,11 +5,11 @@ CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
-CREATE TABLE department (
+CREATE TABLE department_id (
     
-    id INT PRIMARY KEY NOT NULL,
-    department_name VARCHAR(30) NOT NULL,   
-    PRIMARY KEY (id)   
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id),   
 );
 
 CREATE TABLE company_role (
@@ -19,13 +19,35 @@ CREATE TABLE company_role (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE company_employee (
     id INT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-)
+);
+
+
+---adding starting information--
+
+INSERT INTO department (name)
+VALUES("Operations"),
+("Training"),
+("Accounting"),
+("Maintenance"),
+("Security"),
+("Public Relations"),
+("Plundering");
+
+INSERT INTO company_role (title, salary, department_id)
+VALUES ("Owner",5000000000, 1),
+("Landscaping",50000, 4),
+("Patrol",90000,5),
+("News-letter-editor",100000,6),
+("Captain",1000000, 1000);
+
+INSERT INTO company_employee (last_name, first_name, role_id, manager_id)
+VALUES ("Juan", "Flores", 1, NULL);
 
 
 
