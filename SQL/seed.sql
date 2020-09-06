da099f2a2edf8d1485b2a0a79f525db8ef311b8c
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-05 23:10:41 -0400

    added code for tracker.js, added code for seed.sql

--- SQL/seed.sql
+++ SQL/seed.sql
@@ -7,9 +7,29 @@ USE employeeTracker_db;
 
 CREATE TABLE department (
     
-    department_id INT PRIMARY KEY NOT NULL,
-    department_name VARCHAR(30) NOT NULL,      
+    id INT PRIMARY KEY NOT NULL,
+    department_name VARCHAR(30) NOT NULL,   
+    PRIMARY KEY (id)   
+);
 
+CREATE TABLE company_role (
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE employee (
+    id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
 )
+
+
+
+
+
+
 
--- tracker.js
+++ tracker.js
@@ -1,5 +1,86 @@
 const mysql = require("mysql")
 const inquirer = require("inquirer")
-const consoletable = require("console.table")
+const consoletable = require("console.table");
+const { ReadStream } = require("fs");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 8707,
+
+    user: "EASYMAK777",
+
+    password: "Starwood7",
+    database: "employeeTracker_db"
+
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId + "\n");
+    addEmployee();
+});
+
+function addEmployee() {
+    console.log("Adding new Employee....\n");
+    var query = connection.query(
+        "INSERT INTO employee SET ?",
+        {
+            first_name: "Juan Flores",
+            last_name: "Flores",
+            role: "Owner",
+            role_id: 1,
+            manager_id: ""
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + "New employee insterted!\n");
+            updateEmployee();
+        }
+    );
 
 
+    console.log(query.sql);
+}
+
+
+function updateEmployee() {
+    console.log("Updating employee roster...\n");
+    var query = connecton.query(
+        "UPDATE employee SET ? WHERE ?",
+        [
+            {
+                quantity: 1
+            },
+            
+        ],
+        function(err, res) {
+            if(err) throw err;
+            console.log(res.affectedRows + "Employee roster updated!\n");
+            deleteEmployee();
+        }
+    );
+    
+    console.log(query.sql);
+}
+
+function deleteEmployee() {
+    console.log("Deleting outdated employee file...\n");
+    connection.query(
+        "DELETE FROM employee roster WHERE ?",
+        {
+            first_name: "",
+            last_name: "",
+            role: "Owner",
+            role_id: "",
+            manager_id: ""
+
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + "Employee deleted!\n");
+
+            ReadEmployee();
+        }
+    );
+}
