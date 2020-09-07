cee5dac7eaa7b128981690a8e4728fa659361493
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-06 23:04:47 -0400

    added another table, addition data, edited

--- SQL/seed.sql
+++ SQL/seed.sql
@@ -1,24 +1,34 @@
-DROP DATABASE IF EXISTS employeeTracker_db;
-
-CREATE DATABASE employeeTracker_db;
-
-USE employeeTracker_db;
 
 CREATE TABLE company_employee (
-    id INT auto_increment PRIMARY KEY NOT NULL,
+    employee_id INT auto_increment PRIMARY KEY NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    is_currently_employeed BOOLEAN NOT NULL DEFAULT true
+    is_currently_employeed BOOLEAN NOT NULL default true
 );
 
+CREATE TABLE department (
+department_id INT auto_increment PRIMARY KEY NOT NULL,
+department_name varchar(30)
+);
+
+CREATE TABLE role
 
 
 
 
-INSERT INTO company_employee (first_name, last_name, id, role_id, manager_id) 
-VALUES ("Jacob", "Seymore", 1, 1, 1, true);
+
+INSERT INTO company_employee (first_name, last_name, employee_id, role_id, manager_id, is_currently_employeed, department_name); 
+VALUES ("Jacob", "Seymore", 1, 1, 1, true, "sales");
 
 UPDATE company_employee
-SET currently_employeed
+
+
+SELECT
+FROM
+INNER JOIN
+ON
+
+
+
