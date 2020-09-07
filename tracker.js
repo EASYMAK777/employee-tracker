cb23aeae72e28237ca95a105cc15fa29f62a56c9
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-07 19:25:09 -0400

    got the console to react to my selection, it used to break.  Still broken, just breaking in a different way ;)

--- tracker.js
+++ tracker.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-// const consoletable = require("console.table");
+const consoletable = require("console.table");
 // const fs = require("fs");
 
 var connection = mysql.createConnection({
@@ -20,6 +20,7 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId);
     makeConnection();
+    ReadEmployee();
 });
 
 const menu = [
@@ -27,7 +28,9 @@ const menu = [
         name: "Main Menu",
         type: "list",
         message: "What would you like to do?",
-        choices: ["View All Employees",
+        choices: 
+        
+        ["View All Employees",
             "View all employee by department",
             "View All Employees by Role",
             "Add Employee",
@@ -35,100 +38,112 @@ const menu = [
             "Add Department",
             "Update Employee Role",
             "Exit"],
-    }
-];
-
-function makeConnection() {
-    inquirer.prompt(menu).then(function (userChoice){
-        console.log(userChoice.main);
-        var choices = userChoice.main;
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function addEmployee() {
-    console.log("Adding new Employee....\n");
-    var query = connection.query(
-        "INSERT INTO employee SET ?",
-        {
-            first_name: "Juan Flores",
-            last_name: "Flores",
-            role: "Owner",
-            role_id: 1,
-            manager_id: ""
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + "New employee insterted!\n");
-            updateEmployee();
         }
-    );
-
-
-    console.log(query.sql);
-}
-
-
-function updateEmployee() {
-    console.log("Updating employee roster...\n");
-    var query = connecton.query(
-        "UPDATE employee SET ? WHERE ?",
-        [
-            {
-                quantity: 1
-            },
-
-        ],
-        function(err, res) {
-            if(err) throw err;
-            console.log(res.affectedRows + "Employee roster updated!\n");
-            deleteEmployee();
-        }
-    );
-
-    console.log(query.sql);
-}
-
-function deleteEmployee() {
-    console.log("Deleting outdated employee file...\n");
-    connection.query(
-        "DELETE FROM employee roster WHERE ?",
-        {
-            first_name: "",
-            last_name: "",
-            role: "Owner",
-            role_id: "",
-            manager_id: ""
-
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + "Employee deleted!\n");
-
+    ];
+    
+    function makeConnection() {
+        inquirer.prompt(menu).then(function (userChoice){
+            console.log(userChoice.main);
             ReadEmployee();
-        }
-    );
-}
+            
+        })
+    }
+    
+    
+    
+    
+    
+    
+    
+    function ReadEmployee() {
+        console.log ("Selecting from employee roster...\n");
+        connection.query("SELECT*FROM company_employee", function(err, res){
+            if (err) throw err;
+    
+            console.table(res);
+            makeConnection();
+        })
+    }
 
-function ReadEmployee() {
-    console.log ("Selecting from employee roster...\n");
-    connection.query("SELECT*FROM employees", function(err, res){
-        if (err) throw err;
 
-        console.log(res);
-        connection.end();
-    })
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addEmployee() {
+//     console.log("Adding new Employee....\n");
+//     var query = connection.query(
+//         "INSERT INTO company_employee SET ?",
+//         {
+//             first_name: "Juan Flores",
+//             last_name: "Flores",
+//             role: "Owner",
+//             role_id: 1,
+//             manager_id: ""
+//         },
+//         function (err, res) {
+//             if (err) throw err;
+//             console.log(res.affectedRows + "New employee insterted!\n");
+//             updateEmployee();
+//         }
+//     );
+
+
+//     console.log(query.sql);
+// }
+
+
+// function updateEmployee() {
+//     console.log("Updating employee roster...\n");
+//     var query = connecton.query(
+//         "UPDATE company_employee SET ? WHERE ?",
+//         [
+//             {
+//                 quantity: 1
+//             },
+
+//         ],
+//         function(err, res) {
+//             if(err) throw err;
+//             console.log(res.affectedRows + "Employee roster updated!\n");
+//             deleteEmployee();
+//         }
+//     );
+
+//     console.log(query.sql);
+// }
+
+// function deleteEmployee() {
+//     console.log("Deleting outdated employee file...\n");
+//     connection.query(
+//         "DELETE FROM company_employee WHERE ?",
+//         {
+//             first_name: "",
+//             last_name: "",
+//             role: "Owner",
+//             role_id: "",
+//             manager_id: ""
+
+//         },
+//         function (err, res) {
+//             if (err) throw err;
+//             console.log(res.affectedRows + "Employee deleted!\n");
+
+//             ReadEmployee();
+//         }
+//     );
+// }
+
