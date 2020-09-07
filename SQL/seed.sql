8f9a8155fda3f71fef49c90ec3ef858e17276c33
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-06 22:34:55 -0400

    added boolean to employee field

--- SQL/seed.sql
+++ SQL/seed.sql
@@ -1,25 +1,3 @@
-
-DROP DATABASE IF EXISTS employeeTracker_db;
-
-CREATE DATABASE employeeTracker_db;
-
-USE employeeTracker_db;
-
--- CREATE TABLE department_id (
-    
---     id INT NOT NULL,
---     name VARCHAR(30) NOT NULL,
---     PRIMARY KEY (id),   
--- );
-
--- CREATE TABLE company_role (
---     id INT NOT NULL,
---     title VARCHAR(30),
---     salary DECIMAL,
---     PRIMARY KEY (id)
--- );
-
-
 DROP DATABASE IF EXISTS employeeTracker_db;
 
 CREATE DATABASE employeeTracker_db;
@@ -31,26 +9,16 @@ CREATE TABLE company_employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    is_currently_employeed BOOLEAN NOT NULL
 );
 
 
 
 
+
 INSERT INTO company_employee (first_name, last_name, id, role_id, manager_id) 
-VALUES ("Jacob", "Seymore", 3, 1, 1);
+VALUES ("Jacob", "Seymore", 1, 1, 1, true);
 
-SELECT*FROM company_employee;
-
-
-
-
-
-
--- SELECT (first_name,last_name, id)
--- FROM (first_name,last_name,id)
-
--- RIGHT JOIN authors ON books.authorId = authors.id;
--- LEFT JOIN authors ON books.authorId = authors.id;
--- INNER JOIN authors ON books.authorId = authors.id;
-
+UPDATE company_employee
+SET currently_employeed
