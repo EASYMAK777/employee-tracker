e9d7dba026cf72cf45886fa10b3a0ed40fc8e73f
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-06 20:43:34 -0400

    added INSTERT INTO, SELECT*FROM etc SQL commands

--- SQL/seed.sql
+++ SQL/seed.sql
@@ -5,19 +5,19 @@ CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
-CREATE TABLE department_id (
+-- CREATE TABLE department_id (
     
-    id INT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id),   
-);
+--     id INT NOT NULL,
+--     name VARCHAR(30) NOT NULL,
+--     PRIMARY KEY (id),   
+-- );
 
-CREATE TABLE company_role (
-    id INT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE company_role (
+--     id INT NOT NULL,
+--     title VARCHAR(30),
+--     salary DECIMAL,
+--     PRIMARY KEY (id)
+-- );
 
 CREATE TABLE company_employee (
     id INT PRIMARY KEY NOT NULL,
@@ -30,27 +30,12 @@ CREATE TABLE company_employee (
 
 ---adding starting information--
 
-INSERT INTO department (name)
-VALUES("Operations"),
-("Training"),
-("Accounting"),
-("Maintenance"),
-("Security"),
-("Public Relations"),
-("Plundering");
+INSERT INTO company_employee(first_name, last_name, role_id, manager_id) VALUES ("Jacob", "Seymore", 3, 1);
 
-INSERT INTO company_role (title, salary, department_id)
-VALUES ("Owner",5000000000, 1),
-("Landscaping",50000, 4),
-("Patrol",90000,5),
-("News-letter-editor",100000,6),
-("Captain",1000000, 1000);
+SELECT*FROM company_employee
 
-INSERT INTO company_employee (last_name, first_name, role_id, manager_id)
-VALUES ("Juan", "Flores", 1, NULL);
-
-
-
+SELECT first_name,last_name, id
+FROM first_name,last_name,id
 
 
 
--- tracker.js
+++ tracker.js
@@ -17,7 +17,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id" + connection.threadId + "\n");
+    console.log("connected as id" + connection.threadId);
     connection.end();
 });
 
